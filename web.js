43200457622fcd0f0cac2f909332b6ad1edce635
David Chang <zeta11235813@gmail.com>
2011-12-09 00:10:39 +0800

    init

---
+++ web.js
@@ -0,0 +1,54 @@
+var express = require("express");
+var jsdom = require("jsdom");
+var app = express.createServer(express.logger());
+var format = function(num)
+{
+  if(num >= 10)return num.toString();
+  else return "0" + num.toString();
+};
+var url = "http://mindcity.sina.com.tw/MC_data/west/MC-12horos/day/";
+var map = {
+  "capricorn":["0120", "0218"],
+  "pisces":["0219", "0320"],
+  "aries":["0321", "0419"],
+  "taurus":["0420", "0520"],
+  "gemini":["0521", "0621"],
+  "cancer":["0622", "0722"],
+  "leo": ["0723", "0822"],
+  "virgo": ["0823", "0922"],
+  "libra": ["0923", "1023"],
+  "scorpio": ["1024", "1122"],
+  "sagittarius": ["1123", "1221"]
+};
+
+app.get('/',function(req,res){
+  var month = req.param("month"), day = req.param("day");
+  //console.log("req");
+  //console.log(req);
+  //console.log("--->", month);
+  //console.log("--->", day);
+  if(month === undefined || day === undefined)res.send("Oops",404);
+  var date = format(month) + format(day);
+  var now = new Date()
+  var today = now.getFullYear() + format(now.getMonth()+1) + format(now.getDate());
+  var match = "capricorn";
+  for(i in map){
+    if(date >= map[i][0] && date <= map[i][1]){
+      match = i;
+      break;
+    }
+  }
+  //console.log(url + today + "/" + match + ".shtml\n");
+  jsdom.env({html:url + today + "/" + match + ".shtml",
+    done:function(errors,window){
+      var content = window.document.getElementsByClassName('lotconts')[0].innerHTML.replace("<br>", "");
+      res.send(content);
+    }
+  });
+});
+
+var port = process.env.PORT || 4000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
